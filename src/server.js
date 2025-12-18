be7a393766f208d1abe85df82f20d360388cc991
Mohamed Rifayath <141167029+mohamedrifa@users.noreply.github.com>
2025-12-18 11:55:45 +0530

    cors

--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 
+dotenv.config();
 const app = express();
-connectDB();
+app.use(cors({ origin: process.env.CLIENT_URI, credentials: true }));
+app.use(bodyParser.json());
 
-app.use(cors());
-app.use(express.json());
+app.use(cookieParser());
+
+connectDB();
 
 app.use("/api/auth", require("./route/AuthRoute"));
 app.use("/api/user", require("./route/UserRoute"));
@@ -15,6 +20,4 @@ app.use("/api/words", require("./route/WordRoute"));
 app.use("/api/pdf", require("./route/PDFRoute"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
